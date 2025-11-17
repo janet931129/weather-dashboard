b85c4eff24efc890d18e118ddb1dacaaec2ad950
janet931129 <janet112993@gmail.com>
2025-11-18 00:02:55 +0800

    weather_dashboard.py

--- weather_dashboard.py
+++ weather_dashboard.py
@@ -1,14 +1,10 @@
 import requests
-import urllib3
-
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 API_KEY = "你的API_KEY"
 CITY = "Taipei"
-
 url = f"https://opendata.cwa.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization={API_KEY}&locationName={CITY}"
 
 res = requests.get(url, verify=False)
 
-print("HTTP Status:", res.status_code)
-print("Response Text:", res.text[:500])  # 只印前500字
+print("HTTP 狀態碼:", res.status_code)
+print("前500字回傳內容:", res.text[:500])
